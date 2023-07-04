8a693ccc55377601069b04a2ad3cdbce3ebf7a29
Jay Enquito <82007401+jenquito@users.noreply.github.com>
2023-07-04 01:44:06 -0700

    Add files via upload

---
+++ Blurb Game/js/word-db.js
@@ -0,0 +1,350 @@
+let wordList = [
+    {
+        word: "banana",
+        hint: "a minion's favorite food"
+    },
+    {
+        word: "candy",
+        hint: "Aaron Carter - I Want ..."
+    },
+    {
+        word: "github",
+        hint: "web-based version control and collaboration"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "iphone",
+        hint: "you probably have one of these phones"
+    },
+    {
+        word: "gpu",
+        hint: "computer component"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "venice",
+        hint: "famous city of waters"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "dance",
+        hint: "when you hear Cardi B on the radio"
+    },
+    {
+        word: "eagle",
+        hint: "bird of prey"
+    },
+    {
+        word: "grape",
+        hint: "small fruit"
+    },
+    {
+        word: "honey",
+        hint: "sweet syrup"
+    },
+    {
+        word: "jelly",
+        hint: "fruit spread"
+    },
+    {
+        word: "lemon",
+        hint: "sour citrus"
+    },
+    {
+        word: "melon",
+        hint: "sweet fruit"
+    },
+    {
+        word: "olive",
+        hint: "small fruit"
+    },
+    {
+        word: "peach",
+        hint: "soft fruit"
+    },
+    {
+        word: "doku",
+        hint: "poison in japanese"
+    },
+    {
+        word: "rabbit",
+        hint: "furry animal"
+    },
+    {
+        word: "spider",
+        hint: "eight-legged bug"
+    },
+    {
+        word: "tiger",
+        hint: "big cat"
+    },
+    {
+        word: "uncle",
+        hint: "father's brother"
+    },
+    {
+        word: "virus",
+        hint: "sickness"
+    },
+    {
+        word: "water",
+        hint: "clear liquid"
+    },
+    {
+        word: "xenon",
+        hint: "chemical element"
+    },
+    {
+        word: "yacht",
+        hint: "boat"
+    },
+    {
+        word: "bread",
+        hint: "baked food"
+    },
+    {
+        word: "cheese",
+        hint: "dairy product"
+    },
+    {
+        word: "donkey",
+        hint: "horse relative"
+    },
+    {
+        word: "eggnog",
+        hint: "holiday drink"
+    },
+    {
+        word: "fridge",
+        hint: "appliance"
+    },
+    {
+        word: "guitar",
+        hint: "stringed instrument"
+    },
+    {
+        word: "hockey",
+        hint: "ice sport"
+    },
+    {
+        word: "igloo",
+        hint: "snow house"
+    },
+    {
+        word: "jacket",
+        hint: "clothing item"
+    },
+    {
+        word: "kettle",
+        hint: "water boiler"
+    },
+    {
+        word: "leaves",
+        hint: "tree part"
+    },
+    {
+        word: "mantis",
+        hint: "insect"
+    },
+    {
+        word: "noodle",
+        hint: "pasta"
+    },
+    {
+        word: "orange",
+        hint: "citrus fruit"
+    },
+    {
+        word: "pepper",
+        hint: "spice"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+    {
+        word: "iran",
+        hint: "developing country name"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "tesla",
+        hint: "unit of magnetic flux density"
+    },
+    {
+        word: "mars",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "proxy",
+        hint: "related to server application"
+    },
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "air",
+        hint: "related to a gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "key",
+        hint: "small piece of metal"
+    },
+    {
+        word: "egypt",
+        hint: "a country name"
+    },
+    {
+        word: "joker",
+        hint: "psychological thriller film"
+    },
+    {
+        word: "dubai",
+        hint: "developed country name"
+    },
+    {
+        word: "photo",
+        hint: "representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "largest river in the world"
+    },
+    {
+        word: "rain",
+        hint: "related to a water"
+    },
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "it means 'Kin' in Japanese"
+    },
+    {
+        word: "ebay",
+        hint: "online shopping site"
+    },
+    {
+        word: "fartlek",
+        hint: "when runners change between sprint and jog"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    {
+        word: "matrix",
+        hint: "science fiction movie"
+    },
+    {
+        word: "bugs",
+        hint: "you would hate this crawling up to you"
+    },
+    {
+        word: "avatar",
+        hint: "they ruined this movie back in 2010"
+    },
+    {
+        word: "gif",
+        hint: "a file format for image"
+    },
+    {
+        word: "mental",
+        hint: "another word for daft"
+    },
+    {
+        word: "map",
+        hint: "navigate the world using this"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor ice game"
+    },
+    {
+        word: "chess",
+        hint: "the queen's gambit"
+    },
+    {
+        word: "viber",
+        hint: "a social media app"
+    },
+    {
+        word: "myspace",
+        hint: "2003 social media platform"
+    },
+    {
+        word: "xmas",
+        hint: "holiday"
+    },
+    {
+        word: "amongus",
+        hint: "someone is sus"
+    },
+]
